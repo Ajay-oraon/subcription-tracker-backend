ba8d4fed0fcc14fa291f86c3d7eb4a70fc9e9bf1
Ajay-oraon <jaioraon17@gmail.com>
2025-05-22 13:20:07 +0530

    inital version

---
+++ routes/subscription.routes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { createSubscription, getUserSubscriptons } from "../controllers/subscription.controlller.js";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.post("/",authorize, createSubscription);
+subscriptionRouter.get("/", (req, res) => {
+  res.send({ title: "GET All subscription details" });
+});
+subscriptionRouter.get("/:id", (req, res) => {
+  res.send({ title: "GET a subscription details" });
+});
+subscriptionRouter.put("/:id", (req, res) => {
+  res.send({ title: "UPDATE a subscription details" });
+});
+subscriptionRouter.delete("/:id", (req, res) => {
+  res.send({ title: "DELETE a subscription details" });
+});
+subscriptionRouter.get("/user/:id", authorize,getUserSubscriptons);
+subscriptionRouter.put("/:id/cancel", (req, res) => {
+  res.send({ title: "CANCLE a subscription details" });
+});
+subscriptionRouter.put("/upcoming-renewals", (req, res) => {
+  res.send({ title: "GET upcoming renewals" });
+});
+export default subscriptionRouter;
